0c40250235b22bdcc36f983cbe10628bbbc0981e
guige <guige@email.com>
2026-04-10 05:53:14 +0800

    bump: v3.09 - type sorting, share link edit

--- server.js
+++ server.js
@@ -2142,7 +2142,7 @@ self.addEventListener('push', (event) => {
             rss: Math.round(memUsage.rss / 1024 / 1024),
             heapUsed: Math.round(memUsage.heapUsed / 1024 / 1024)
           },
-          version: 'v3.08',
+          version: 'v3.09',
         });
         return;
       }
