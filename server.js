c2a0269c565da1d0b80736a311ee3635bc7e1709
guige <guige@email.com>
2026-04-10 06:56:22 +0800

    chore: version bump v3.22 → v3.30

--- server.js
+++ server.js
@@ -2247,7 +2247,7 @@ self.addEventListener('push', (event) => {
             rss: Math.round(memUsage.rss / 1024 / 1024),
             heapUsed: Math.round(memUsage.heapUsed / 1024 / 1024)
           },
-          version: 'v3.22',
+          version: 'v3.30',
         });
         return;
       }
