cd16c52ecf932400f3039a62534e43c1765c070c
guige <guige@email.com>
2026-04-10 05:22:23 +0800

    bump: v3.04

--- server.js
+++ server.js
@@ -2078,7 +2078,7 @@ self.addEventListener('push', (event) => {
             rss: Math.round(memUsage.rss / 1024 / 1024),
             heapUsed: Math.round(memUsage.heapUsed / 1024 / 1024)
           },
-          version: 'v3.03',
+          version: 'v3.04',
         });
         return;
       }
